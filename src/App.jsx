503e87ec6103e062eaa2a1fb725f3ac4d13b4f26
ShivanshKothari <shivanshkothari2004@gmail.com>
2024-07-31 19:36:51 +0530

    Version 0.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,82 @@
 import { useState } from "react";
+import InputBox from "./components/InputBox";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import "./App.css";
+import { useCallback } from "react";
 
 function App() {
+  const [ amount, setAmount ] = useState(0);
+  const [ from, setFrom ] = useState("usd");
+  const [ to, setTo ] = useState("inr");
+  const [ convertedAmount, setConvertedAmount ] = useState(0);
+  const currencyInfo = useCurrencyInfo(from);
+  const currencyOptions = Object.keys(currencyInfo);
+
+  const swapCurrency = useCallback(() => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  });
+
+  const convertAmount = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
+
   return (
-    <>
-      <div
-        className="flex content-center justify-center p-4 h-screen w-full"
-        style={{
-          backgroundImage:
-            "url('https://imgs.search.brave.com/tRge9BkGhTtfG8hrVclB1NBbiP__ROMIDb0p_B5BBXI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTU3/MzgwMjM4L3Bob3Rv/L21vc2FpYy1jb2xs/ZWN0aW9uLW9mLXdv/cmxkLWN1cnJlbmNp/ZXMuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPUdUSVN0RnRF/Y2pKcjU1a0czMzRx/cUxLZDIyNTc4Y05p/XzRPRFNHX0ItUlE9')",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-      >
-        <div>
-          <h1 className="bg-white/80 pt-12 px-20 pb-2 rounded-t-3xl font-bold text-blue-700 drop-shadow-2xl self-start text-3xl text-center">
-            Currency Convertor
-          </h1>
-          <div className="bg-white/80 flex-col p-4">
-            <div></div>
-            <div>oarisenstoiarsn</div>
-          </div>
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('https://imgs.search.brave.com/DP14wW4MN7wGcbjMmwRiibc5F-q_icr5J7QjIbwcBe8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9lMC5w/eGZ1ZWwuY29tL3dh/bGxwYXBlcnMvODEy/LzIvZGVza3RvcC13/YWxscGFwZXItbW9u/ZXktc3RvY2stbWFy/a2V0LWdhbGxlcnkt/Ymx1ZS1zdG9jay1t/YXJrZXQtZmluYW5j/aWFsLW1hcmtldC5q/cGc')`,
+      }}
+    >
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              convertAmount();
+            }}
+          >
+            <div className="w-full mb-1">
+              <InputBox
+                label="From"
+                amount={amount}
+                currencyOptions={currencyOptions}
+                onCurrencyChange={(currency) => setAmount(currency)}
+                onAmountChange={(amount) => setAmount(amount)}
+                selectCurrency={from}
+              />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                onClick={swapCurrency}
+              >
+                swap
+              </button>
+            </div>
+            <div className="w-full mt-1 mb-4">
+              <InputBox
+                label="To"
+                amount={convertedAmount}
+                currencyOptions={currencyOptions}
+                onCurrencyChange={(currency)=> setTo(currency)}
+                amountDisable={true}
+                selectCurrency={to}
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+            >
+              Convert {from.toUpperCase()} to {to.toUpperCase()}
+            </button>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -3,8 +3,10 @@ import { useState, useEffect } from "react";
 function useCurrencyInfo(currency){
     const [data, setData] = useState({});
     useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.com/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
         .then((res)=>res.json())
-        .then((res)=>setData(res));
-    }, [])
+        .then((res)=>setData(res[currency]));
+    }, [currency])
+    return data;
 }
+export default useCurrencyInfo;
