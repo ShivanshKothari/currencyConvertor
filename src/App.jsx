91de9a6700938abbe1f8188d3779ac822b1775be
ShivanshKothari <shivanshkothari2004@gmail.com>
2024-07-31 14:07:45 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        className="flex content-center justify-center p-4 h-screen w-full"
+        style={{
+          backgroundImage:
+            "url('https://imgs.search.brave.com/tRge9BkGhTtfG8hrVclB1NBbiP__ROMIDb0p_B5BBXI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTU3/MzgwMjM4L3Bob3Rv/L21vc2FpYy1jb2xs/ZWN0aW9uLW9mLXdv/cmxkLWN1cnJlbmNp/ZXMuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPUdUSVN0RnRF/Y2pKcjU1a0czMzRx/cUxLZDIyNTc4Y05p/XzRPRFNHX0ItUlE9')",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div>
+          <h1 className="bg-white/80 pt-12 px-20 pb-2 rounded-t-3xl font-bold text-blue-700 drop-shadow-2xl self-start text-3xl text-center">
+            Currency Convertor
+          </h1>
+          <div className="bg-white/80 flex-col p-4">
+            <div></div>
+            <div>oarisenstoiarsn</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({});
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.com/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then((res)=>setData(res));
+    }, [])
+}
